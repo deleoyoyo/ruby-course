a677c13f020779abb7df8e11289ac4b39963c7d9
deleoyoyo <leonidaschiu@gmail.com>
2021-05-06 19:44:12 -0700

    add ruby methods

--- hello.rb
+++ hello.rb
@@ -1,6 +1,8 @@
 # Lesson 1: Outputting a String
-puts "Hello World!"
+
+puts "What is your name?"
 
 # Lesson 2: Prompting User Input
-name = gets
+
+name = gets.chop
 puts "Hey, #{name}! Nice to meet you!"
